93aa5a83c9162a7b6f02f2fc4eae147ef2dcc01f
FerdiFachrezi <144778972+FerdiFachrezi@users.noreply.github.com>
2024-12-04 13:00:57 +0700

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/cart.css" />
+  </head>
+  <body>
+  <header>
+        <div class="logo">
+            <img src="./assets/logo.png" alt="">
+            <title>Your Cart</title>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="#categories">Categories</a></li>
+                <li><a href="#products">Products</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+            <div class="profile">
+                <a href=""><img src="./assets/profil.png" alt=""></a>
+            </div>
+        </nav>
+    </header>
+    <div class="desktop">
+      <div class="div">
+        <div class="overlap-group">
+          <div class="group">
+            <div class="text-wrapper">ORDER SUMMARY</div>
+            <img class="line" src="img/line-2.svg" />
+            <div class="text-wrapper-2">4 PRODUCTS</div>
+          </div>
+          <div class="text-wrapper-3">Product subtotal</div>
+          <div class="text-wrapper-4">Rp. 55.000</div>
+          <div class="text-wrapper-5">Delivery fees</div>
+          <div class="text-wrapper-6">Rp. 5.500</div>
+          <div class="text-wrapper-7">Total</div>
+          <div class="text-wrapper-8">Rp. 60.500</div>
+          <div class="text-wrapper-9">TERMS OF USE</div>
+          <p class="p">By clicking the payment button, you agree to our terms and conditions.</p>
+          <div class="overlap"><div class="text-wrapper-10">PROCEED TO PAYMENT</div></div>
+          <div class="div-wrapper"><div class="text-wrapper-11">CONTINUE SHOPPING</div></div>
+        </div>
+        <div class="overlap-2">
+          <div class="image"></div>
+            <img class="img" src="foto/chitato.png" />
+        </div>
+        <div class="overlap-3">
+          <div class="text-wrapper-12">SIMPLE MART</div>
+          <img class="untitled-design" src="img/untitled-design-1.png" />
+          <div class="text-wrapper-13">Home</div>
+          <div class="text-wrapper-14">Products</div>
+          <div class="text-wrapper-15">Categories</div>
+          <div class="text-wrapper-16">Contact Us</div>
+          <img class="group-2" src="img/group-1.png" />
+        </div>
+        <div class="overlap-4">
+          <div class="image-2"></div>
+          <img class="image-3" src="foto/bango.png" />
+        </div>
+        <div class="text-wrapper-17">MY CARTS</div>
+        <img class="line-2" src="img/line-1.svg" />
+        <p class="text-wrapper-18">Chitato Snack rasa keju 68 g</p>
+        <div class="bango-kecap-manis">Bango Kecap Manis 135mL</div>
+        <div class="text-wrapper-19">Rp. 11.000</div>
+        <div class="text-wrapper-20">Rp. 11.500</div>
+        <div class="frame">
+          <div class="text-wrapper-21">Quantity :</div>
+          <div class="frame-2">
+            <div class="frame-wrapper">
+              <div class="frame-3"><div class="text-wrapper-22">-</div></div>
+            </div>
+            <div class="frame-4"><div class="text-wrapper-23">2</div></div>
+            <div class="frame-wrapper">
+              <div class="frame-3"><div class="text-wrapper-22">+</div></div>
+            </div>
+          </div>
+        </div>
+        <div class="frame-5">
+          <div class="text-wrapper-21">Quantity :</div>
+          <div class="frame-2">
+            <div class="frame-wrapper">
+              <div class="frame-3"><div class="text-wrapper-22">-</div></div>
+            </div>
+            <div class="frame-4"><div class="text-wrapper-23">2</div></div>
+            <div class="frame-wrapper">
+              <div class="frame-3"><div class="text-wrapper-22">+</div></div>
+            </div>
+          </div>
+        </div>
+        <div class="text-wrapper-24">Subtotal : 22.000</div>
+        <div class="text-wrapper-25">Subtotal : 23.000</div>
+      </div>
+    </div>
+  </body>
+</html>
